8ee3232f6ae760722767f95b5d40b3bb8a6f6942
Jordan Roberts <jrobertswebdev@gmail.com>
2024-03-05 15:29:35 -0600

    disable ws

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from 'express'
-import { WebSocketServer} from 'ws'
+import { WebSocketServer } from 'ws'
 import dotenv from 'dotenv'
 import { v4 as uuidv4 } from 'uuid'
 import { uploadImageToCloudinary } from './lib/services.js'
 import { getSuggest } from './utils.js'
 import { client, upscaleImage } from './lib/midjourney.js'
 
-import http from 'http';
+import http from 'http'
 
 dotenv.config()
 const app = express()
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 app.use(express.json())
 
 const wss = new WebSocketServer({ server })
@@ -21,7 +21,7 @@ wss.on('connection', function connection(ws) {
   console.log('connected')
   const id = uuidv4()
   clients[id] = ws
-  ws.send(JSON.stringify({ id:id }))
+  ws.send(JSON.stringify({ id: id }))
 
   ws.on('close', function close() {
     delete clients[id]
@@ -40,21 +40,20 @@ app.post('/suggest', async (req, res) => {
   res.send(suggestions)
 })
 
-
 app.post('/var', async (req, res) => {
   const data = req.body
   const { job, index, wsId } = data
-  try{
+  try {
     let prog = 0
     const update = (progress) => {
       if (clients[wsId]) {
         const ws = clients[wsId]
-        ws.send(JSON.stringify({status: progress}))
+        ws.send(JSON.stringify({ status: progress }))
       }
     }
-   const makeVariations = async (job, index) => {
-      client.Close()
-      await client.init()
+    const makeVariations = async (job, index) => {
+      // client.Close()
+      // await client.init()
       const data = JSON.parse(job)
       console.log('data', data)
       try {
@@ -65,8 +64,8 @@ app.post('/var', async (req, res) => {
           flags: data.flags,
           content: data.content,
           // content: prompt, //remix mode require content
-          loading: (uri, progress) => {            
-              prog = prog < 76 ? prog + 23: 100    
+          loading: (uri, progress) => {
+            prog = prog < 76 ? prog + 23 : 100
             update(`${prog}%`)
             console.log('loading', uri, 'progress', prog)
           },
@@ -77,41 +76,43 @@ app.post('/var', async (req, res) => {
         return { error: error.message }
       }
     }
-  const response = await makeVariations(job, index)
-  const responseObj = await JSON.parse(response)
-// const responseObj = tinyLizardWizard
-  const imgData = await uploadImageToCloudinary(responseObj.uri, responseObj.content, 'style')
-  console.log('from server', imgData, JSON.stringify(responseObj) )
-  res.send({
-    meta:await responseObj,
-    imgData: imgData
-  }).status(200)
-} catch (error) {
-  console.log(error)
-  res.send(error).status(500)
-}
-})
-
-app.post('/upscale', async (req, res) => {
-  const data = req.body
-  const { job, index } = data
-  try{
-    const response = await upscaleImage(job, index)
+    const response = await makeVariations(job, index)
     const responseObj = await JSON.parse(response)
-  // const responseObj = tinyLizardWizard
+    // const responseObj = tinyLizardWizard
     const imgData = await uploadImageToCloudinary(responseObj.uri, responseObj.content, 'style')
-    console.log('from server', imgData, JSON.stringify(responseObj) )
-    res.send({
-      meta:await responseObj,
-      imgData: imgData
-    }).status(200)
+    console.log('from server', imgData, JSON.stringify(responseObj))
+    res
+      .send({
+        meta: await responseObj,
+        imgData: imgData,
+      })
+      .status(200)
   } catch (error) {
     console.log(error)
     res.send(error).status(500)
   }
 })
 
-
+app.post('/upscale', async (req, res) => {
+  const data = req.body
+  const { job, index } = data
+  try {
+    const response = await upscaleImage(job, index)
+    const responseObj = await JSON.parse(response)
+    // const responseObj = tinyLizardWizard
+    const imgData = await uploadImageToCloudinary(responseObj.uri, responseObj.content, 'style')
+    console.log('from server', imgData, JSON.stringify(responseObj))
+    res
+      .send({
+        meta: await responseObj,
+        imgData: imgData,
+      })
+      .status(200)
+  } catch (error) {
+    console.log(error)
+    res.send(error).status(500)
+  }
+})
 
 app.post('/gen', async (req, res) => {
   const data = req.body
@@ -120,11 +121,11 @@ app.post('/gen', async (req, res) => {
     const update = (progress) => {
       if (clients[wsId]) {
         const ws = clients[wsId]
-        ws.send(JSON.stringify({status: progress}))
+        ws.send(JSON.stringify({ status: progress }))
       }
     }
     const generateMj = async (prompt) => {
-      await client.init()
+      // await client.init()
       try {
         const job = await client.Imagine(prompt, (uri, progress) => {
           update(progress)
@@ -139,19 +140,19 @@ app.post('/gen', async (req, res) => {
     const responseObj = JSON.parse(response)
     const imgData = await uploadImageToCloudinary(responseObj.uri, responseObj.content, 'style')
 
-    res.send({
-              meta: await responseObj,
-              imgData: imgData,
-              caption: prompt,
-            }).status(200)
+    res
+      .send({
+        meta: await responseObj,
+        imgData: imgData,
+        caption: prompt,
+      })
+      .status(200)
     console.log(imgData)
   } catch (error) {
     console.log(error)
     res.status(500).send(error)
   }
 })
-
-
 
 const PORT = process.env.PORT || 8888
 server.listen(PORT, () => {
