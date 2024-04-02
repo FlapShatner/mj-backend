c8fade9c40e8d64baeb3889d1fb28615b0d6238f
Jordan Roberts <jrobertswebdev@gmail.com>
2024-04-02 10:39:03 -0500

    handle and convey content violation error

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use('/suggest', suggestRoute)
 app.use('/', api)
 
 app.get('/', (req, res) => {
- res.send('Hello From  MJ Backend')
+    res.send('Hello From  MJ Backend')
 })
 
 const wss = new WebSocketServer({ server })
@@ -25,180 +25,187 @@ const connections = {}
 const users = {}
 
 const handleClose = (uuid) => {
- console.log(`connection ${uuid} closed`)
- delete connections[uuid]
- broadcast()
+    console.log(`connection ${uuid} closed`)
+    delete connections[uuid]
+    broadcast()
 }
 
 const broadcast = () => {
- Object.keys(connections).forEach((uuid) => {
-  const connection = connections[uuid]
-  const message = JSON.stringify(users)
-  connection.send(message)
- })
+    Object.keys(connections).forEach((uuid) => {
+        const connection = connections[uuid]
+        const message = JSON.stringify(users)
+        connection.send(message)
+    })
 }
 
 wss.on('connection', (connection, req) => {
- console.log('connected')
- const id = uuidv4()
- connections[id] = connection
- connection.send(JSON.stringify({ event: 'id', id: id }))
- connection.on('close', () => handleClose(id))
- connection.on('message', (message) => handleMessage(message, id))
+    console.log('connected')
+    const id = uuidv4()
+    connections[id] = connection
+    connection.send(JSON.stringify({ event: 'id', id: id }))
+    connection.on('close', () => handleClose(id))
+    connection.on('message', (message) => handleMessage(message, id))
 })
 const update = (progress, id) => {
- console.log('update', progress, id)
- if (connections[id]) {
-  const ws = connections[id]
-  ws.send(JSON.stringify({ event: 'status', status: progress }))
- }
+    console.log('update', progress, id)
+    if (connections[id]) {
+        const ws = connections[id]
+        ws.send(JSON.stringify({ event: 'status', status: progress }))
+    }
 }
 
 const sendResults = (resultsObj) => {
- const { id } = resultsObj
- if (connections[id]) {
-  const ws = connections[id]
-  ws.send(JSON.stringify(resultsObj))
- }
+    const { id } = resultsObj
+    if (connections[id]) {
+        const ws = connections[id]
+        ws.send(JSON.stringify(resultsObj))
+    }
 }
 
 const sendError = (error, id) => {
- if (connections[id]) {
-  const ws = connections[id]
-  ws.send(JSON.stringify({ event: 'error', error: error }))
- }
+    if (connections[id]) {
+        const ws = connections[id]
+        ws.send(JSON.stringify({ event: 'error', error: error }))
+    }
 }
 
 const handleGenerate = async (message, id) => {
- const { prompt, shape, caption } = message.data
- if (!prompt) return
- await client.init()
- // console.log('prompt', prompt)
- const response = await makeGenerate(prompt, id)
-
- const responseObj = await JSON.parse(response)
- const imgData = await uploadImageToCloudinary(responseObj.uri, responseObj.content)
- const resultsObj = {
-  imgData: imgData,
-  meta: response,
-  id: id,
-  shape: shape,
-  caption: caption,
-  prompt: prompt,
-  event: 'generate',
- }
- sendResults(resultsObj)
- console.log('response', response)
+    const { prompt, shape, caption } = message.data
+    if (!prompt) return
+    await client.init()
+    // console.log('prompt', prompt)
+    const response = await makeGenerate(prompt, id)
+    console.log('response', response)
+    if (response.error) {
+        sendError(response.error, id)
+        return
+    }
+    const responseObj = await JSON.parse(response)
+    const imgData = await uploadImageToCloudinary(responseObj.uri, responseObj.content)
+    const resultsObj = {
+        imgData: imgData,
+        meta: response,
+        id: id,
+        shape: shape,
+        caption: caption,
+        prompt: prompt,
+        event: 'generate',
+    }
+    sendResults(resultsObj)
 }
 
 const handleUpscale = async (message, id) => {
- console.log('upscale message recieved from ', id)
- const { meta, activeIndex, shape, prompt, caption, style } = message.data
- const response = await makeUpscale(meta, activeIndex)
- const responseObj = await JSON.parse(response)
- console.log('responseObj', responseObj)
- if (responseObj.error) {
-  sendError(responseObj.error, id)
-  return
- }
- const imgData = await uploadImageToCloudinary(responseObj.uri, responseObj.content, style)
- const resultsObj = {
-  imgData: imgData,
-  meta: response,
-  id: id,
-  shape: shape,
-  caption: caption,
-  prompt: prompt,
-  style: style,
-  event: 'upscale',
- }
- sendResults(resultsObj)
+    console.log('upscale message recieved from ', id)
+    const { meta, activeIndex, shape, prompt, caption, style } = message.data
+    const response = await makeUpscale(meta, activeIndex)
+    const responseObj = await JSON.parse(response)
+    console.log('responseObj', responseObj)
+    if (responseObj.error) {
+        sendError(responseObj.error, id)
+        return
+    }
+    const imgData = await uploadImageToCloudinary(responseObj.uri, responseObj.content, style)
+    const resultsObj = {
+        imgData: imgData,
+        meta: response,
+        id: id,
+        shape: shape,
+        caption: caption,
+        prompt: prompt,
+        style: style,
+        event: 'upscale',
+    }
+    sendResults(resultsObj)
 }
 
 const makeUpscale = async (job, index) => {
- await client.init()
- const data = JSON.parse(job)
- try {
-  const upscale = await client.Upscale({
-   index: index,
-   msgId: data.id,
-   hash: data.hash,
-   flags: data.flags,
-   loading: (uri, progress) => {
-    console.log('loading', uri, 'progress', progress)
-   },
-  })
-  return JSON.stringify(upscale)
- } catch (error) {
-  return { error: error.message }
- }
+    await client.init()
+    const data = JSON.parse(job)
+    try {
+        const upscale = await client.Upscale({
+            index: index,
+            msgId: data.id,
+            hash: data.hash,
+            flags: data.flags,
+            loading: (uri, progress) => {
+                console.log('loading', uri, 'progress', progress)
+            },
+        })
+        return JSON.stringify(upscale)
+    } catch (error) {
+        return { error: error.message }
+    }
 }
 
 const makeGenerate = async (prompt, id) => {
- await client.init()
- const job = await client.Imagine(prompt, (uri, progress) => {
-  update(progress, id)
-  console.log('loading', uri, 'progress', progress)
- })
- return JSON.stringify(job)
+    try {
+        await client.init()
+        const job = await client.Imagine(prompt, (uri, progress) => {
+            update(progress, id)
+            console.log('loading', uri, 'progress', progress)
+        })
+        return JSON.stringify(job)
+    } catch (error) {
+        return { error: error.message }
+    }
 }
 
 const handleVariations = async (message, id) => {
- const dataObj = { ...message.data, id: id, event: 'variations' }
- const response = await makeVariations(dataObj)
- const responseObj = JSON.parse(response)
- const imgData = await uploadImageToCloudinary(responseObj.uri, responseObj.content, dataObj.style)
- const resultsObj = {
-  imgData: imgData,
-  meta: response,
-  id: id,
-  event: 'variations',
-  shape: dataObj.shape,
-  caption: dataObj.caption,
-  prompt: dataObj.prompt,
-  style: dataObj.style,
- }
- sendResults(resultsObj)
+    const dataObj = { ...message.data, id: id, event: 'variations' }
+    const response = await makeVariations(dataObj)
+    const responseObj = JSON.parse(response)
+    const imgData = await uploadImageToCloudinary(responseObj.uri, responseObj.content, dataObj.style)
+    const resultsObj = {
+        imgData: imgData,
+        meta: response,
+        id: id,
+        event: 'variations',
+        shape: dataObj.shape,
+        caption: dataObj.caption,
+        prompt: dataObj.prompt,
+        style: dataObj.style,
+    }
+    sendResults(resultsObj)
 }
 
 const makeVariations = async (dataObj) => {
- const { meta, activeIndex, prompt, id } = dataObj
- await client.init()
- const data = JSON.parse(meta)
- try {
-  const variations = await client.Variation({
-   index: activeIndex,
-   msgId: data.id,
-   hash: data.hash,
-   flags: data.flags,
-   content: prompt,
-   loading: (uri, progress) => {
-    update(progress, id)
-    console.log('loading', uri, 'progress', progress)
-   },
-  })
-  return JSON.stringify(variations)
- } catch (error) {
-  return { error: error.message }
- }
+    const { meta, activeIndex, prompt, id } = dataObj
+    await client.init()
+    const data = JSON.parse(meta)
+    try {
+        const variations = await client.Variation({
+            index: activeIndex,
+            msgId: data.id,
+            hash: data.hash,
+            flags: data.flags,
+            content: prompt,
+            loading: (uri, progress) => {
+                update(progress, id)
+                console.log('loading', uri, 'progress', progress)
+            },
+        })
+        return JSON.stringify(variations)
+    } catch (error) {
+        return { error: error.message }
+    }
 }
 
 const handleMessage = async (bytes, id) => {
- const message = JSON.parse(bytes)
- // console.log('message', message)
- if (message.data) {
-  if (message.event === 'generate') {
-   handleGenerate(message, id)
-  } else if (message.event === 'variations') {
-   handleVariations(message, id)
-  } else if (message.event === 'upscale') {
-   handleUpscale(message, id)
-   console.log('received upscale req')
-  }
- }
+    const message = JSON.parse(bytes)
+    // console.log('message', message)
+    if (message.data) {
+        if (message.event === 'generate') {
+            handleGenerate(message, id)
+        } else if (message.event === 'variations') {
+            handleVariations(message, id)
+        } else if (message.event === 'upscale') {
+            handleUpscale(message, id)
+            console.log('received upscale req')
+        }
+    }
 }
 
 const PORT = process.env.PORT || 8888
 server.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
